e17a4b111334eb2d5f2b5495d5f074151e46c6fd
mintdunno <tnm1042002@gmail.com>
2024-02-22 19:12:57 +0700

    hladsfjls

--- public/js/play.js
+++ public/js/play.js
@@ -635,3 +635,24 @@ function decrementTimer() {
         }, 1000);
     }
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("QRcode");
+var modalImg = document.getElementById("img01");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
